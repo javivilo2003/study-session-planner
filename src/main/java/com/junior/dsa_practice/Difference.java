38ed020adaf9222ca1b9544c53992ef0893d05be
javivilo2003 <jave1403@gmail.com>
2026-08-09 22:59:53 +0200

    DSA exercise completed from wednesday. Find the Difference.

---
+++ src/main/java/com/junior/dsa_practice/Difference.java
@@ -0,0 +1,36 @@
+package com.junior.dsa_practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Difference {
+
+    public static Character findTheDifference(String s, String t){
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);   
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return c;
+            } else {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) < 0) {
+                    return c;
+                }
+            }
+        }
+
+        return null;        
+    }
+
+    public static void main(String[] args) {
+        String s = "aabb";
+        String t = "ababb";
+
+        System.out.println(findTheDifference(s, t));
+
+    }    
+}
